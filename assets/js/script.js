a907eac4b449203e4b58b16ca7f8f986e36bbef3
trymysniper13 hi <nicowilliam13@gmail.com>
2021-07-23 21:56:53 -0500

    yes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-buttonEl.addEventListener("click", function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.ClassName = "take-item" ;
-    listItemEl.textContent = "this is a new task. ";
-    tasksToDoEl.appendChild(listItemEl);
-});
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  };
-  buttonEl.addEventListener("click", createTaskHandler);
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
+
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+  formEl.addEventListener("submit", createTaskHandler);
   
 
 
