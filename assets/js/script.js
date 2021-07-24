a3112324a761cf6a5a9fc666780f52efca2d420d
trymysniper13 hi <nicowilliam13@gmail.com>
2021-07-23 20:22:37 -0500

    yes

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
+buttonEl.addEventListener("click", function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.ClassName = "take-item" ;
+    listItemEl.textContent = "this is a new task. ";
+    tasksToDoEl.appendChild(listItemEl);
+});
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  }
+
+
